d6385e091912e806966d48a158be14602163b341
Valerie Campbell <valeriecs10@gmail.com>
2020-06-24 19:42:02 -0700

    reformulated move_piece in Board to work on exception handling

--- board.rb
+++ board.rb
@@ -9,74 +9,45 @@ class Board
         set_pieces if fill_board
     end
     
-    def set_pieces
-        @rows[0] = row_of_mixed_pieces(@rows[0], 0, :white)
-        @rows[1] = row_of_pawns(@rows[1], 1, :white)
-        @rows[6] = row_of_pawns(@rows[6], 6, :black)
-        @rows[7] = row_of_mixed_pieces(@rows[7], 7, :black)
-    end
-
-    def row_of_mixed_pieces(row, row_i, color)
-        [
-            Rook.new(color, self, [row_i, 0]),
-            Knight.new(color, self, [row_i, 1]),
-            Bishop.new(color, self, [row_i, 2]),
-            Queen.new(color, self, [row_i, 3]),
-            King.new(color, self, [row_i, 4]),
-            Bishop.new(color, self, [row_i, 5]),
-            Knight.new(color, self, [row_i, 6]),
-            Rook.new(color, self, [row_i, 7])
-        ]
-    end
-
-    def row_of_pawns(row, row_i, color)
-        row.each_with_index.map { |space, col| Pawn.new(color, self, [row_i, col]) }
-    end
-
-    def row_of_null_pieces(row)
-        row.each_with_index.map { |space| NullPiece.instance }
-    end
-
     def [](pos)
         x, y = pos
         @rows[x][y]
     end
-
+    
     def []=(pos, val)
         x, y = pos
         @rows[x][y] = val
     end
-
+    
     def move_piece(start_pos, end_pos, color = nil)
         piece = self[start_pos]
-        raise "No piece at start position" if self[start_pos].is_a?(NullPiece)
-        raise "Not a valid move" if !valid_pos?(start_pos) || 
-                !piece.valid_moves.include?(end_pos)
+        validate_input(start_pos, end_pos)
+        
         self[end_pos] = self[start_pos] 
-
+            
         # assign new position to piece that was moved
         self[end_pos].pos = end_pos
-
+            
         self[start_pos] = NullPiece.instance
     end
-
+        
     def valid_pos?(pos)
         return false if pos.any? { |i| !i.between?(0, 7) }
         true
     end
-
+        
     def add_piece(piece, color, pos)
         raise "Not a valid pos" unless valid_pos?(pos)
         self[pos] = piece.new(color, self, pos)
     end
-
+        
     def checkmate?(color)
         return false if !in_check?(color)
         pieces.select { |piece| piece.color == color }.all? do |piece|
-           piece.valid_moves.empty?
+            piece.valid_moves.empty?
         end
     end
-
+        
     def in_check?(color)
         king_pos = find_king(color)
         opposing_color = color == :black ? :white : :black
@@ -84,12 +55,12 @@ class Board
             return true if piece.moves.include?(king_pos)
         end
     end
-
+        
     def find_king(color)
         pieces.each { |piece| return piece.pos if piece.is_a?(King) && 
-                        piece.color == color }
+        piece.color == color }
     end
-
+        
     def pieces
         pieces = []
         @rows.each do |row|
@@ -99,7 +70,7 @@ class Board
         end
         pieces
     end
-
+        
     def dup
         temp = Board.new(false)
         temp.rows.each_with_index do |row, row_i|
@@ -112,18 +83,57 @@ class Board
         end
         temp
     end
-
+        
     def move_piece!(start_pos, end_pos, color = nil)
         piece = self[start_pos]
         raise "No piece at start position" if self[start_pos].is_a?(NullPiece)
         raise "Not a valid move" if !valid_pos?(start_pos) || !piece.moves.include?(end_pos)
         self[end_pos] = self[start_pos] 
-
+            
         # assign new position to piece that was moved
         self[end_pos].pos = end_pos
-
+            
         self[start_pos] = NullPiece.instance
     end
+        
+    private
 
+    def validate_input(start_pos, end_pos)
+        begin
+            raise "No piece at start position" if self[start_pos].is_a?(NullPiece)
+            raise "Not a valid move" if !valid_pos?(start_pos) || 
+            !piece.valid_moves.include?(end_pos)
+        rescue
+            # report error here and loop again?
+        end
+    end
+
+    def set_pieces
+        @rows[0] = row_of_mixed_pieces(@rows[0], 0, :white)
+        @rows[1] = row_of_pawns(@rows[1], 1, :white)
+        @rows[6] = row_of_pawns(@rows[6], 6, :black)
+        @rows[7] = row_of_mixed_pieces(@rows[7], 7, :black)
+    end
+    
+    def row_of_mixed_pieces(row, row_i, color)
+        [
+            Rook.new(color, self, [row_i, 0]),
+            Knight.new(color, self, [row_i, 1]),
+            Bishop.new(color, self, [row_i, 2]),
+            Queen.new(color, self, [row_i, 3]),
+            King.new(color, self, [row_i, 4]),
+            Bishop.new(color, self, [row_i, 5]),
+            Knight.new(color, self, [row_i, 6]),
+            Rook.new(color, self, [row_i, 7])
+        ]
+    end
+    
+    def row_of_pawns(row, row_i, color)
+        row.each_with_index.map { |space, col| Pawn.new(color, self, [row_i, col]) }
+    end
+    
+    def row_of_null_pieces(row)
+        row.each_with_index.map { |space| NullPiece.instance }
+    end
 
 end
