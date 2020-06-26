e6a99c0da3525b52c724b69f4bea54ec440b2a8c
Valerie Campbell <valeriecs10@gmail.com>
2020-06-25 19:46:59 -0700

    added basic Game and HumanPlayer classes. Needs debugging.

---
+++ game.rb
@@ -0,0 +1,37 @@
+require_relative 'board'
+require_relative 'human_player'
+
+class Game
+    attr_reader :board, :players, :display, :current_player
+    def initialize(player1, player2)
+        @board = Board.new
+        @display = Display.new(board)
+        @players = {:white => player1.new(board), :black => player2.new(board)}
+        @current_player = :white
+    end
+
+    def play
+        display.render
+        until players.any? { |player| board.checkmate?(player) } do
+
+            begin
+                @players[current_player].make_move(board)
+            rescue => error
+                puts error.message
+                sleep 5
+            end 
+
+            swap_turn!
+        end
+    end
+
+    private
+
+    def swap_turn!
+        if current_player == :white
+            @current_player = :black
+        else
+            @current_player = :white
+        end
+    end
+end
---
+++ human_player.rb
@@ -0,0 +1,38 @@
+require_relative 'display'
+require_relative 'board'
+
+class HumanPlayer
+    attr_reader :board, :display
+
+    def initialize(board)
+        @board = board
+        @display = Display.new(board)
+    end
+
+    def make_move(board)
+        start_pos = []
+        end_pos = []
+        
+        until end_pos != start_pos do
+            start_pos = []
+            end_pos = []
+            start_pos = select_input
+            end_pos = select_input
+        end
+
+        board.move_piece(start_pos, end_pos)
+    end
+    
+    private
+    
+    def select_input
+        input = nil
+    
+        while input.nil?
+            input = display.cursor.get_input
+            display.render
+        end
+        
+        return input
+    end
+end
